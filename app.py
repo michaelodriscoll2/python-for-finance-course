c88470b122e73ba4308ef44a480bbb2dc6a31416
michaelodriscoll2 <michael.odriscoll2@ucdconnect.ie>
2025-10-13 18:08:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="Intro to Programming - Project", layout="wide")
+st.title("Intro to Programming - Project")
+
+st.write("Dashboard start")
+
+
+
 
